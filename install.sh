f7b5952747400854594d7ff16255cb767aa5437a
Chris Edwards <screwtop.github@ripples.dyndns.org>
2016-01-10 10:44:56 +1300

    Added rudimentary install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TARGET=/usr/local/bin
+
+sudo cp -v g-copy.sh $TARGET/g-copy
+sudo cp -v g-dis.tcl $TARGET/g-dis
+sudo cp -v g-split.tcl $TARGET/g-split
+sudo cp -v g-stats.tcl $TARGET/g-stats
+sudo cp -v g-strip.tcl $TARGET/g-strip
+sudo cp -v frame.tcl $TARGET/g-rect
+
+sudo chmod 775 $TARGET/g-*
